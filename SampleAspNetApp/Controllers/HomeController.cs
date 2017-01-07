02ee654877ca8f0863ef3dda42cef737b9c8aa59
Erik Dahl <edahl@nwpsc.com>
2017-01-07 13:32:27 -0600

    initial javascript app

---
+++ SampleAspNetApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace SampleAspNetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult JavaScriptPage()
+        {
+            return View();
+        }
+
+        public ActionResult TypeScriptPage()
+        {           
+            return View();
+        }        
+    }
+}
---
+++ SampleAspNetApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleAspNetApp.MvcApplication" Language="C#" %>
